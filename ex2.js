ef52e21f59d0d0766994e52f24f54cfef0d04836
daramedeiros <daraamedeiros@outlook.com>
2019-10-02 22:23:29 -0300

    exercicios aula 03

--- ex2.js
+++ ex2.js
@@ -22,7 +22,7 @@ console.log(arr)
 //   [ 15, 14, 13, 12, 11 ],
 //   [ 10, 9, 8, 7, 6 ],
 //   [ 5, 4, 3, 2, 1 ] ]
-const arr = []
+const arra = []
 let count = 25
 for (let i = 0; i < 5; i++) {
   const linha = []
@@ -30,9 +30,9 @@ for (let i = 0; i < 5; i++) {
     linha[j] = count
     count--
   }
-  arr[i] = linha
+  arra[i] = linha
 }
-console.log(arr)
+console.log(arra)
 
 // 3 - Retorne o valor mais alto das somas entre o total de valors por linha, de uma array 4x4.
 //const arr4x4 = [[23, 56, 9, 0], [8, 24, 50, 5], [9, 10, 40, 7], [9, 48, 15, 2]]
@@ -41,7 +41,7 @@ console.log(arr)
 // 5 6 6 7
 // 9 9 9 9
 // Resultado esperado -> 36
-const arr = []
+const arra = []
 const arr4x4 = [[23, 56, 9, 0], [8, 24, 50, 5], [9, 10, 40, 7], [9, 48, 15, 2]]
 for (let i = 0; i < arr4x4.length; i++) {
   let total = arr4x4[i].reduce(function(a, b) {
@@ -49,7 +49,7 @@ for (let i = 0; i < arr4x4.length; i++) {
   })
   arr[i] = total
 }
-console.log(Math.max(...arr))
+console.log(Math.max(...arra))
 
 //ou ...
 
@@ -128,21 +128,33 @@ console.log(dadosOrganizados)
 //   [71, 72, 73, 74, 75]
 // ]
 
-//7- const cacaPalavras = [
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "l", "e", "f", "a", "n", "t", "e"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["c", "a", "s", "a", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
-//   ["a", "w", "e", "r", "e", "d", "e", "u", "i", "r"],
-//   ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"]
-// ]
+//7- 
+const palavras = ["elefante", "casa", "rede"]
 
-// const palavras = ["elefante", "casa", "rede"]
+const cacaPalavras = [
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "l", "e", "f", "a", "n", "t", "e"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["c", "a", "s", "a", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"],
+  ["a", "w", "e", "r", "e", "d", "e", "u", "i", "r"],
+  ["a", "w", "e", "r", "t", "y", "u", "u", "i", "r"]
+]
+
+
+for(let i = 0; cacaPalavras.length; i++ ){
+  cacaPalavras[i] = cacaPalavras[i].join("")
+  for(let j = 0; cacaPalavras.length ; j++){
+    if(cacaPalavras[i].indexOf(palavras[j]) > -1) {
+      console.log(` A palavra ${palavras[j]} está na linha ${i + 1} e começa na coluna ${cacaPalavras[i].indexOf(palavras[j] +1)}` )
+    }
+  }
+}
+
 
 // ache as palavras da array palavras na array cacaPalavras e diga em qual linha e em qual coluna
 // elas estao.
--- ex3.js
+++ ex3.js
@@ -1,3 +1,5 @@
+const r = require("readline-sync")
+
 //objetos
 
 // 1.
@@ -8,21 +10,49 @@
 // com pelo menos 4 propriedades e 1 método
 // ref: https://pt.wikipedia.org/wiki/Lista_de_Pok%C3%A9mon
 
+const pokemon = {}
+
+pokemon.nome = "Pikachu"
+pokemon.nomeJapones = "Pikachu"
+pokemon.numEmJapones = 022
+pokemon.ev = "Pichu"
+
+pokemon.falar = function () {
+    console.log('Pika, Pika')
+}
+
+console.log(` Esse é o nome do meu pokemom escolhido ${pokemon.nome}, ele tem o mesmo nome em Japones ${pokemon.nomeJapones} e o seu ev é ${pokemon.ev} \n Ele sempre fala:`)
+
+console.log(pokemon.falar())
+
 // 2.
 // Printe no console a seguinte
 // frase: "Oi, meu nome é  < >.
 // e meu nome japones é < >."
+
+console.log(`Oi meu nome é: ${pokemon.nome} e meu nome em japones é ${pokemon.nomeJapones}! HAHAHA`)
 
 // 3.
 // Adicione uma nova propriedade ao
 // seu pokemon chamada `sabe_voar`
 // (ou algo do tipo) e defina
 // ela como `true` ou `false`.
+pokemon.sabe_voar = false
 
 // 4.
 // Adicione um método ao seu pokemon
 // chamado `voar` que retorna "Desculpa, nao sei voar." ou "
 // To voaaaaaando!".
+pokemon.voar = function (sabe_voar) {
+    if (sabe_voar == true) {
+        console.log("To voaaaaaando!")
+    } else {
+        console.log("Desculpa, nao sei voar.")
+    }
+}
+
+console.log(pokemon.voar())
+    
 
 // 5.
 // Adicione um método chamado
